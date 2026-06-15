9d1623849496ab54f7c4c617d5c8ceeb34a536d7
theol <theo.lafonhocine@gmail.com>
2026-06-16 00:01:11 +0200

    premiere visualisation plotly capteur 2

--- analyse.py
+++ analyse.py
@@ -24,4 +24,27 @@ nom_colonnes = ["id_moteur", "cycle", "reglage_1", "reglage_2", "reglage_3"] \
 df_propre = df_brut.toDF(*nom_colonnes)
 
 print("Apercu du DataFrame")
-df_propre.show(5)
+df_propre.show(10)
+
+df_moteur1= df_propre.filter(df_propre.id_moteur==1)
+df_p = df_moteur1.select("cycle", "capteur_2").toPandas()
+
+
+import plotly.express as px
+fig = px.line(df_p, x="cycle", y="capteur_2", title="Capteur 2 vs moteur 1")
+fig.write_html("capteur_2_moteur_1.html")
+
+import plotly.graph_objects as go
+
+moteurs = df_propre.select("id_moteur").distinct().toPandas()["id_moteur"].tolist()
+#la ligne ci dessus permet de recuperer la liste des moteurs DISTINCTS ( uniques ) du dataframe et de la mettre en liste 
+fig = go.Figure()# on cree une figure vide
+
+for moteur in moteurs:
+    df_m = df_propre.filter(df_propre.id_moteur == moteur).select("cycle", "capteur_2").toPandas()
+    fig.add_trace(go.Scatter(x=df_m["cycle"], y=df_m["capteur_2"], mode="lines", name=f"Moteur {moteur}"))
+
+fig.update_layout(title="Capteur 2 pour tous les moteurs", xaxis_title="Cycle", yaxis_title="Valeur")
+fig.write_html("capteur2_tous_moteurs.html")
+
+
